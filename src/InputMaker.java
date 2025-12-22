76bcbdf31baee9e38d40387ce627ff630b02ed2f
JolieBao <@>
2025-12-22 10:19:24 -0500

    input wip

--- src/InputMaker.java
+++ src/InputMaker.java
@@ -3,7 +3,10 @@ public class InputMaker {
 
     public InputMaker() {
         for (int i = 0; i < Math.pow(120,2); i++) {
-            
+            double r = Math.random();
+            if (r <= 0.25){
+                input += "O";
+            }
             if (i % 120 == 0){
                 input += "\n";
             }
